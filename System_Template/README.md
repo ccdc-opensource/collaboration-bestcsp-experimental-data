308103c593da79a41b7a61e4ee0f738fc84b58e9
Isaac Sugden <isugden@ccdc.cam.ac.uk>
2024-06-17 13:53:00 +0100

    IJS data for Paris meeting discussion NO_JIRA

--- System_Template/README.md
+++ System_Template/README.md
@@ -1,12 +1,24 @@
 # System Template
 
 This is a template directory for an example system in the BEST-CSP project.
-There should be a folder for each property associated with the system, and a csv file
-that records the data, alongside appropriate metadata for how the data was generated.
+There should be a csv file for each polymorph, and each physical property for that polymorph, containing the experimental results,
+alongside appropriate metadata for how the data was generated.
 There is absolutely no expectation that every property will be filled in for each system,
 but please try to include as much detail as possible for the data that is recorded.
+The first line in the csv will be the REFCODE of the best representative structure for that system. The
+second line will be the column titles, typically taking the format:
+Identifier, Property, Value, Std, N, Name, Comment, Reference
 
+- Identifer: A persistent identifier so that users can discuss individual results
+- Property: The property, e.g. Melting point
+- Value (Units): The measurand recorded in the experiment. Units will typically be K, kJ/mol
+- Std: The Standard deviation of the value, if the contribution is a full dataset
+- N: The number of experiments performed if the contribution is a full dataset
+- Name: Contributor's name
+- Reference/Author: If the value is from the literature, then leave a DOI, otherwise, an identifier for the set of unpublished data.
+For example, I have used M_Dudek_2024.1 for the data contributed by Marta Dudek at the Zagreb meeting in March 2024
 There should be a README.md in each System folder, with high level details about the system
+- Comments: Anything the contributor considers pertinent to the reported value (pressure, ambient conditions etc.); please avoid commas and quote marks!
 
 ## Submitting data
 
@@ -17,7 +29,8 @@ and we can update the repo, but please try to include metadata either way
 
 ## Naming Conventions
 
-csv files should be named system_property.csv
+csv files should be named in a way that unequivocably identifies the polymorph, e.g., Monoclinic_beta_II_melting_point.csv
+if there is more than one way of referring to the polymorph
 
 ## README.md contents
 
---
+++ stats.py
@@ -0,0 +1,89 @@
+# imports
+import glob, sys, os
+import numpy as np
+from statsmodels.stats.meta_analysis import combine_effects
+
+
+files = glob.glob('%s\\*.csv' % (sys.argv[1]))
+print("Found", len(files), "csv files:")
+for filename in files: print(filename)
+
+
+
+for filename in files:
+    mean_effect = np.array([])
+    var_effect = np.array([])
+    #nobs = np.array([]) # Number of observations for each lab.
+    # And names/labels
+    idx = []
+    print ("Reading", filename)
+    f = open(filename, 'r')
+    data = f.readlines()
+    f.close()
+    ndict = dict()
+    for line in data[2:]:
+        #print([s.strip() for s in line.split(',')])
+        id, prop, value, s, n, author = [s.strip() for s in line.split(',')][:6]
+        # Find out how many data points there are for each lab... 
+        if author not in ndict:
+            if n == '':
+                ndict[author] = 1
+            else:
+                ndict[author] = int(n) 
+        else:
+            if n == '':
+                ndict[author] += 1
+            else:
+                ndict[author] += int(n)           
+    combined_names = []        
+    combined_values = [] 
+    for name in ndict:
+        values = []
+        print(name, "has", ndict[name], "data points")
+        if ndict[name] > 3:
+            for line in data[2:]:
+                id, prop, value, s, n, author = [s.strip() for s in line.split(',')][:6]
+                if author == name:
+                    values.append(float(value))
+            mean_effect = np.append(mean_effect, np.mean(values))        
+            var_effect = np.append(var_effect, np.var(np.array(values)))        
+            idx.append(name)
+            print(name, "OK, data added")    
+        else:
+            # Could be too few data or there is a standard deviation. 
+            for line in data[2:]:
+                #print([s.strip() for s in line.split(',')][:6])
+                id, prop, value, s, n, author = [s.strip() for s in line.split(',')][:6]
+                if author == name:
+                    if s != '':
+                        print ("has standard deviation")
+                        mean_effect = np.append(mean_effect, np.array(float(value)))
+                        var_effect = np.append(var_effect, np.array((float(s)**2)))
+                        idx.append(name)
+                        print(name, "OK, data added.")
+                    else:
+                        combined_names.append(name)
+                        combined_values.append(float(value))
+    if len(combined_values) > 2:
+        idx.append(', '.join(set(combined_names)))
+        print("Combining data for", set(combined_names))
+        mean_effect = np.append(mean_effect, np.mean(combined_values))
+        var_effect = np.append(var_effect, np.var(combined_values))
+ 
+    print ("mean_effect", mean_effect)
+    print ("var_effect", var_effect)
+    print("Calculating consensus for", filename)
+    results = combine_effects(mean_effect, var_effect, method_re="pm", row_names=idx)
+    #results.conf_int_samples(nobs=nobs)
+    print("Heterogeneity, tau-square:", results.tau2)
+    print(results.summary_frame())  
+    print("Testing homogeneity:")
+    homo = results.test_homogeneity()
+    if homo.pvalue < 0.05:
+        print("Warning, significant inhomogeneity detected!")
+    print (homo)    
+    fig = results.plot_forest()
+    fig.tight_layout()
+    fig.savefig(filename.replace('.csv', '') + '.png')
+    # Plot results
+    
